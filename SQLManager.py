dd924c840a3593cced20461edf00848bfaa50a1d
KYU <131807399+Kyussy@users.noreply.github.com>
2024-05-11 11:57:18 +0300

    id_branch commit 2

--- SQLManager.py
+++ SQLManager.py
@@ -12,7 +12,8 @@ class SQLManaging():
         self.cr.execute('''
                             CREATE TABLE IF NOT EXISTS Todo_list (
                             done INTEGER,
-                            text TEXT NOT NULL
+                            text TEXT NOT NULL,
+                            id INTEGER PRIMARY KEY
                             )
                             ''')
         self.db.commit()
@@ -28,9 +29,9 @@ class SQLManaging():
                 done = True
             else:
                 done = False
-            records_formated.append({"text":records[i-1][1], "done":done})
+            records_formated.append({"text":records[i-1][1], "done":done, "id":records[i-1][2]})
         return records_formated
-    def create_part(self, done=False, textt=str):
+    def create_part(self, textt=str, done=False):
         self.cr.execute('''INSERT INTO Todo_list(done, text) values(?,?)''', [done, textt])
         self.db.commit()
 
@@ -43,9 +44,26 @@ class SQLManaging():
         self.db.commit()
         self.create_tables()
         for i in range(len(new_info)):
-            self.cr.execute('''INSERT INTO Todo_list(done, text) values(?, ?)''', [new_info[i-1]["done"],new_info[i-1]["text"]])
+            self.cr.execute('''INSERT INTO Todo_list(done, text) values(?, ?, ?)''', [new_info[i-1]["done"],new_info[i-1]["text"], new_info[i-1]["id"]])
         self.db.commit()
+    def drop_table(self, table=str):
+        self.cr.execute('DROP TABLE '+table)
+        self.db.commit()
+    def execute_func(self):
+        function_list = ["create tables", "get info", "create part", "drop table"]
+        which = input(("which function?"+ str(function_list)))
+
+
+        while which not in function_list:
+            which = input("not in list or bad pronounc., which function?")
+        if which == "create tables":
+            self.create_tables()
+        elif which == "get info":
+            print(self.get_info())
+        elif which == "create part":
+            self.create_part(textt=input("text"), done=int(input("Done or not? (1 or 0)")))
+        elif which == "drop table":
+            self.drop_table(input("Which table?(current tables: Todo_list)"))
 
 db = SQLManaging("todolist.db")
-db.create_tables()
 #TODO get real
